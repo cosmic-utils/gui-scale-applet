4967fdb194864569735717840a0b1517eb1f4d61
Bryan Hyland <bryan.hyland32@gmail.com>
2025-02-12 23:29:15 -0800

    Update main.rs to handle setting up tray/panel icons on other DEs.

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod config;
 mod logic;
 mod window;
 pub(crate) mod de;
+mod tray;
 
+use crate::tray::setup_tray_icon;
 use crate::window::Window;
 
 fn main() -> cosmic::iced::Result {
+    // Setup the tray/panel icon for the DEs.
+    setup_tray_icon();
+    // Run the applet
     cosmic::applet::run::<Window>(())?;
 
     Ok(())
